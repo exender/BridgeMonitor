645939ace6854250c7fabb50e94feb42a0358170
exender <baptistebonnin4@gmail.com>
2021-04-02 13:01:16 +0200

    ajout de tout les fichiers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BridgeMonitor.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace BridgeMonitor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<BoatModel> boats = GetBoats();
+            boats.Sort((s1, s2) => DateTimeOffset.Compare(s1.ClosingDate, s2.ClosingDate));
+
+            foreach (var boatModel in boats)
+            {
+                if (DateTimeOffset.Compare(DateTimeOffset.Now, boatModel.ClosingDate) < 0)
+                {
+                    ViewData["Boat"] = boatModel;
+                    break;
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            ViewData["Boats"] = GetBoats();
+            return View();
+        }
+
+      
+
+        public static List<BoatModel> GetBoats()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<BoatModel>>(stringResult.Result);
+                return result;
+            }
+        }
+    }
+}
---
+++ Models/BoatModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BridgeMonitor.Models
+{
+    public class BoatModel
+    {
+        [JsonProperty("boat_name")]
+        public string BoatName { get; set; }
+
+        [JsonProperty("closing_type")]
+        public string ClosingType { get; set; }
+
+        [JsonProperty("closing_date")]
+        public DateTime ClosingDate { get; set; }
+
+        [JsonProperty("reopening_date")]
+        public DateTime ReopeningDate { get; set; }
+    }
+}
